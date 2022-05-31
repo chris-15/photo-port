46fd5f879ea525e203ce5ca82ad02a4711ebee62
Chris <christopher.sarm15@gmail.com>
2022-05-31 15:23:57 -0400

    render gallery pages

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,7 +5,10 @@ function About () {
     return (
         <section className='my-5'>
             <h1 id='about'>Who am I?</h1>
-            <img src={coverImage} className='my-2' style ={{ width: "100%" }} alt="cover" />   
+            <img src={coverImage} className='my-2' style ={{ width: "100%" }} alt="cover" />  
+            <div className='my-2'>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quis nulla suscipit, error consequatur aperiam voluptatem illo optio, nemo unde, veniam autem nam nihil sint libero ipsum? Natus, quas excepturi? </p>
+            </div> 
         </section>
     );
 }
