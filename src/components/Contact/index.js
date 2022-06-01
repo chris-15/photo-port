4e399595a93e9b7cece20367a5048db0240dd16b
Chris <christopher.sarm15@gmail.com>
2022-06-01 14:06:57 -0400

    conditional rendering and test for nav and contact elements

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -36,8 +36,6 @@ function ContactForm() {
     if (!errorMessage) {
       setFormState({ ...formState, [e.target.name]: e.target.value });
     }
-
-    
   }
 
   // console.log(formState);
@@ -49,7 +47,7 @@ function ContactForm() {
 
   return (
     <section>
-      <h1>Contact me</h1>
+      <h1 data-testid="h1tag">Contact me</h1>
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
@@ -79,11 +77,13 @@ function ContactForm() {
           />
         </div>
         {errorMessage && (
-            <div>
-                <p className="error-text">{errorMessage}</p>
-            </div>
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
         )}
-        <button type="submit">submit</button>
+        <button data-testid="button" type="submit">
+          Submit
+        </button>
       </form>
     </section>
   );
--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -8,6 +8,8 @@ const categories = [
 ];
 const mockCurrentCategory = jest.fn();
 const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 
 afterEach(cleanup);
 
@@ -18,6 +20,9 @@ describe("Nav component", () => {
         categories={categories}
         setCurrentCategory={mockSetCurrentCategory}
         currentCategory={mockCurrentCategory}
+        contactSelected={mockContactSelected}
+        setContactSelected={mockSetContactSelected}
+
     />);
   });
   //snapshot test
@@ -27,6 +32,8 @@ describe("Nav component", () => {
         categories={categories}
         setCurrentCategory={mockSetCurrentCategory}
         currentCategory={mockCurrentCategory}
+        contactSelected={mockContactSelected}
+        setContactSelected={mockSetContactSelected}
     />);
     expect(asFragment()).toMatchSnapshot();
   });
@@ -38,6 +45,8 @@ describe("emoji is visible", () => {
         categories={categories}
         setCurrentCategory={mockSetCurrentCategory}
         currentCategory={mockCurrentCategory}
+        contactSelected={mockContactSelected}
+        setContactSelected={mockSetContactSelected}
     />);
 
     expect(getByLabelText("camera")).toHaveTextContent("📸");
@@ -50,6 +59,8 @@ describe("links are visible", () => {
         categories={categories}
         setCurrentCategory={mockSetCurrentCategory}
         currentCategory={mockCurrentCategory}
+        contactSelected={mockContactSelected}
+        setContactSelected={mockSetContactSelected}
     />);
     expect(getByTestId("link")).toHaveTextContent("Oh Snap!");
     expect(getByTestId("about")).toHaveTextContent("About Me");
